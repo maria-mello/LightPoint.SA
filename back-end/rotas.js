d7ba518ece6bcb159cf6b0809f4f6f3072d47529
MARIA EDUARDA MELLO <maria_mello1@estudante.sesisenai.org.br>
2025-10-20 09:32:03 -0300

    subindo espaço ao redor

--- back-end/rotas.js
+++ back-end/rotas.js
@@ -1,50 +1,49 @@
 // aqui vou colocar todas as rotas que vão estar recebendo requisiçoes vindas do front-end
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const dotenv = require('dotenv');
+const cors = require("cors");
+const dotenv = require("dotenv");
 dotenv.config();
 
-const db = require('./dbService');
+const db = require("./db");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Rota POST 
-app.post('/insert', (request, response) => {
-
-  console.log("Chegando algo do front? Mostre: ", request)
+// Rota POST
+app.post("/insert", (request, response) => {
+  console.log("Chegando algo do front? Mostre: ", request);
   const result = db.insertNewName(request.body);
   result
-    .then(data => response.json({ data: data }))
-    .catch(err => console.error(err));
+    .then((data) => response.json({ data: data }))
+    .catch((err) => console.error(err));
 });
 
 // Rota GET(Read)
-app.get('/getAll', (request, response) => {
+app.get("/getAll", (request, response) => {
   const result = db.getAllData();
   result
-    .then(data => response.json({ data: data }))
-    .catch(err => console.error(err));
+    .then((data) => response.json({ data: data }))
+    .catch((err) => console.error(err));
 });
 
 // ROTA PATCH(ATUALIZAR/EDUTAR)
-app.patch('/update', (request, response) => {
+app.patch("/update", (request, response) => {
   const result = db.updateNameById(request.body);
   result
-    .then(data => response.json({ success: data }))
-    .catch(err => console.error(err));
+    .then((data) => response.json({ success: data }))
+    .catch((err) => console.error(err));
 });
 
 // ROTA DELETE(DELETAR/EXCLUIR)
-app.delete('/delete/:id', (request, response) => {
+app.delete("/delete/:id", (request, response) => {
   const { id } = request.params;
 
   const result = db.deleteRowById(id);
   result
-    .then(data => response.json({ success: data }))
-    .catch(err => console.error(err));
+    .then((data) => response.json({ success: data }))
+    .catch((err) => console.error(err));
 });
 
 // // ROTA GET(LER/BUSCAR)
